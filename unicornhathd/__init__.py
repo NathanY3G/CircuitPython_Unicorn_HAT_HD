b8c84348128ff1193d956e676a0b44aea093f202
Nathan Young <77929198+NathanY3G@users.noreply.github.com>
2021-10-29 20:41:58 +0200

    Initial version

---
+++ unicornhathd/__init__.py
@@ -0,0 +1,5 @@
+# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
+# SPDX-FileCopyrightText: Copyright (c) 2021 Nathan Young
+#
+# SPDX-License-Identifier: MIT
+from .unicornhathd import UnicornHATHD
